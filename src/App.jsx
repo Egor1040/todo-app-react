3d344bfea43e1df94ded3672700e1f140c9b9593
Igor <igor.motriy3@gmail.com>
2023-08-24 22:02:07 +0300

    implemented edit elem

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,29 @@ import AddTable from './components/AddTable/AddTable';
 
 function App() {
     const [data, setData] = useState([
-        {id: 'K36Y2LhNS', text: 'JS', icon: 'img/icons8-close-30.png'},
-        {id: 'oO7Z74kGA', text: 'React', icon: 'img/icons8-close-30.png'},
-        {id: 'Q1rvGl3tj', text: 'jQuery', icon: 'img/icons8-close-30.png'}
+        {id: 'K36Y2LhNS', text: 'JS', edit: 'img/icons8-edit-30.png', delete: 'img/icons8-close-30.png'},
+        {id: 'oO7Z74kGA', text: 'React', edit: 'img/icons8-edit-30.png', delete: 'img/icons8-close-30.png'},
+        {id: 'Q1rvGl3tj', text: 'jQuery', edit: 'img/icons8-edit-30.png', delete: 'img/icons8-close-30.png'}
     ]);
 
     const addRow = (text) => {
         if(text) {
-            const newRow = { id: shortid.generate(), text: text, icon: 'img/icons8-close-30.png' };
+            const newRow = { id: shortid.generate(), text: text, edit: 'img/icons8-edit-30.png', delete: 'img/icons8-close-30.png' };
             setData([...data, newRow]);
         }
     };
+
+    const editRow = (id,value) => {
+        const newData = data.map(item => {
+            if(value) {
+                if(item.id === id) {
+                    return {...data, id: id, text: value};
+                }
+            }
+            return item;
+        })
+        setData(newData);
+    }
 
     const deleteRow = (id) => {
         const updatedData = data.filter(item => item.id !== id);
@@ -28,7 +40,7 @@ function App() {
             <h2 className='App-title'>REACT TODO APP</h2>
             <div className="todo-app">
                 <AddHeader onAdd={addRow} />
-                <AddTable data={data} onDelete={deleteRow}/>
+                <AddTable data={data} editRow={editRow} onDelete={deleteRow}/>
             </div>
         </div>
     );
--- src/components/AddTable/AddTable.jsx
+++ src/components/AddTable/AddTable.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddTable.css';
 
-const Table = ({ data, onDelete }) => {
-  return (
-    <table className='main-table'>
-        <tbody className='table-desc'>
-            {data.map(item => (
-                <tr className='desc-items' key={item.id}>
-                    <td className='descr-item descr-item__id'>{item.id}</td>
-                    <td className='descr-item descr-item__text'>{item.text}</td>
-                    <td className='descr-item descr-item__remove'>
-                        <img onClick={() => onDelete(item.id)} src={item.icon} data-id={ item.id } alt=''/>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
-  );
+const Table = ({ data, editRow, onDelete }) => {
+    const [inputText, setInputText] = useState('');
+    const [visib, setVisib] = useState(false);
+    const [editingItemId, setEditingItemId] = useState(null);
+
+    console.log(visib)
+
+    return (
+        <table className='main-table'>
+            <tbody className='table-desc'>
+                {data.map(item => (
+                    <tr className='descr-items' key={item.id}>
+                        <td className='descr-item descr-item__id'>{item.id}</td>
+                        <td className='descr-item descr-item__text'>{item.text}</td>
+                        <td className='descr-item'>
+                            <div className={ !visib ? 'descr-item__icons active' : 'descr-item__icons unactive'}>
+                                <img className='descr-item__edit' onClick={() => {
+                                    setVisib(!visib)
+                                    setEditingItemId(item.id)}} src={ item.edit } alt="" />
+                                <img className='descr-item__remove' onClick={() => onDelete(item.id)} src={item.delete} data-id={ item.id } alt=''/>
+                            </div>
+                            <div className={ `editor-menu ${visib && item.id === editingItemId ? 'active' : ''}`}>
+                                <input type="text"  onChange={(e) => setInputText(e.target.value)} className='editor-menu__input' />
+                                <button onClick={() => {
+                                    editRow(item.id, inputText);
+                                    setInputText(''); 
+                                    setVisib(!visib);
+                                }} className='editor-menu__button'>Edit</button>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 };
 
 export default Table;
